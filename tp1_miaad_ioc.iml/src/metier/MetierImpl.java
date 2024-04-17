8b5148154e9f301666b3a20915bc036996d86b80
brahmdi <96790699+brahmdi@users.noreply.github.com>
2024-04-17 16:00:05 +0100

    Add files via upload

---
+++ tp1_miaad_ioc.iml/src/metier/MetierImpl.java
@@ -0,0 +1,19 @@
+package metier;
+
+import dao.DaoImpl;
+import dao.IDao;
+
+public class MetierImpl implements IMetier{
+    private  IDao dao  ;
+    @Override
+    public double calcul() {
+        double temp = dao.getData() ;
+        double res = temp * 540 /Math.cos(temp * Math.PI) ;
+
+        return res;
+    }
+
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+}
